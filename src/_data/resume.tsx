658651ced8ee60b9425217ba749f8c850039467b
EvansRobbie <evansrobbie5311@gmail.com>
2024-08-26 21:57:23 +0300

    projects details set up

--- src/_data/resume.tsx
+++ src/_data/resume.tsx
@@ -114,4 +114,184 @@ export const DATA = {
       'Directus',
     ],
   },
+  projects: [
+    {
+      title: 'Kai And Karo - Car dealership',
+      href: 'https://www.kaiandkaro.com/',
+      dates: 'Oct 2023 - Present',
+      active: true,
+      description:
+        'A specification for adding human and machine readable meaning to commit messages using color-rich emojis 🌈',
+      technologies: [
+        'git',
+        'chakra ui',
+        'typescript',
+        'react',
+        'next.js',
+        'tailwind css',
+      ],
+      links: [
+        {
+          type: 'Website',
+          href: 'https://www.kaiandkaro.com/',
+          icon: <Icons.smile className='size-3' />,
+        },
+        {
+          type: 'Source',
+          href: 'https://github.com/conventional-emoji-commits/conventional-emoji-commits.site',
+          icon: <Icons.github className='size-3' />,
+        },
+      ],
+      image: '',
+      video: 'https://cdn.nyxbui.design/conventional-emoji-commits.mp4',
+    },
+    {
+      title: 'Bridge lab',
+      href: 'https://bridgelab.africa',
+      dates: 'June 2023 - Present',
+      active: true,
+      description:
+        'Fusion UI library combining Shadcn/UI and MagicUI. Featuring 150+ free and open-source components built with React, Typescript, Tailwind CSS, and Framer Motion. Create stunning, responsive interfaces effortlessly. 100% open-source.',
+      technologies: ['Next.js', 'Typescript', 'TailwindCSS', 'Material UI'],
+      links: [
+        {
+          type: 'Website',
+          href: 'https://bridgelab.africa',
+          icon: <Icons.globe className='size-3' />,
+        },
+      ],
+      image: '',
+      video: 'https://cdn.nyxbui.design/bento-grid.mp4',
+    },
+    {
+      title: 'Onekitty',
+      href: 'https://www.onekitty.co.ke/',
+      dates: 'June 2023 - Present',
+      active: true,
+      description:
+        'Fusion UI library combining Shadcn/UI and MagicUI. Featuring 150+ free and open-source components built with React, Typescript, Tailwind CSS, and Framer Motion. Create stunning, responsive interfaces effortlessly. 100% open-source.',
+      technologies: ['Next.js', 'Typescript', 'TailwindCSS', 'Material UI'],
+      links: [
+        {
+          type: 'Website',
+          href: 'https://www.onekitty.co.ke/',
+          icon: <Icons.globe className='size-3' />,
+        },
+      ],
+      image: '',
+      video: 'https://cdn.nyxbui.design/bento-grid.mp4',
+    },
+    {
+      title: 'Pride of Kenya Awards',
+      href: 'https://www.prideawards.africa/',
+      dates: 'June 2023 - Present',
+      active: true,
+      description:
+        'Fusion UI library combining Shadcn/UI and MagicUI. Featuring 150+ free and open-source components built with React, Typescript, Tailwind CSS, and Framer Motion. Create stunning, responsive interfaces effortlessly. 100% open-source.',
+      technologies: ['Next.js', 'Typescript', 'TailwindCSS', 'Material UI'],
+      links: [
+        {
+          type: 'Website',
+          href: 'https://www.prideawards.africa/',
+          icon: <Icons.globe className='size-3' />,
+        },
+      ],
+      image: '',
+      video: 'https://cdn.nyxbui.design/bento-grid.mp4',
+    },
+    {
+      title: 'Hoteles & Hogares',
+      href: 'https://hoteleshogares.com/',
+      dates: 'June 2023 - Present',
+      active: true,
+      description:
+        'Fusion UI library combining Shadcn/UI and MagicUI. Featuring 150+ free and open-source components built with React, Typescript, Tailwind CSS, and Framer Motion. Create stunning, responsive interfaces effortlessly. 100% open-source.',
+      technologies: ['Next.js', 'Typescript', 'TailwindCSS', 'Material UI'],
+      links: [
+        {
+          type: 'Website',
+          href: 'https://hoteleshogares.com/',
+          icon: <Icons.globe className='size-3' />,
+        },
+      ],
+      image: '',
+      video: 'https://cdn.nyxbui.design/bento-grid.mp4',
+    },
+    {
+      title: 'Mjengo Smart Technology',
+      href: 'https://www.mjengosmart.com/',
+      dates: 'June 2023 - Present',
+      active: true,
+      description:
+        'Fusion UI library combining Shadcn/UI and MagicUI. Featuring 150+ free and open-source components built with React, Typescript, Tailwind CSS, and Framer Motion. Create stunning, responsive interfaces effortlessly. 100% open-source.',
+      technologies: ['Next.js', 'Typescript', 'TailwindCSS', 'Material UI'],
+      links: [
+        {
+          type: 'Website',
+          href: 'https://www.mjengosmart.com/',
+          icon: <Icons.globe className='size-3' />,
+        },
+      ],
+      image: '',
+      video: 'https://cdn.nyxbui.design/bento-grid.mp4',
+    },
+    {
+      title: 'Save The Sicklers',
+      href: 'https://savethesicklers.org/',
+      dates: 'June 2023 - Present',
+      active: true,
+      description:
+        'Fusion UI library combining Shadcn/UI and MagicUI. Featuring 150+ free and open-source components built with React, Typescript, Tailwind CSS, and Framer Motion. Create stunning, responsive interfaces effortlessly. 100% open-source.',
+      technologies: ['Next.js', 'Typescript', 'TailwindCSS', 'Material UI'],
+      links: [
+        {
+          type: 'Website',
+          href: 'https://savethesicklers.org/',
+          icon: <Icons.globe className='size-3' />,
+        },
+      ],
+      image: '',
+      video: 'https://cdn.nyxbui.design/bento-grid.mp4',
+    },
+    {
+      title: 'Farmers Master Class',
+      href: 'https://farmers-master-class-fe.vercel.app/',
+      dates: 'June 2023 - Present',
+      active: true,
+      description:
+        'Fusion UI library combining Shadcn/UI and MagicUI. Featuring 150+ free and open-source components built with React, Typescript, Tailwind CSS, and Framer Motion. Create stunning, responsive interfaces effortlessly. 100% open-source.',
+      technologies: ['Next.js', 'Typescript', 'TailwindCSS', 'Material UI'],
+      links: [
+        {
+          type: 'Website',
+          href: 'https://farmers-master-class-fe.vercel.app/',
+          icon: <Icons.globe className='size-3' />,
+        },
+      ],
+      image: '',
+      video: 'https://cdn.nyxbui.design/bento-grid.mp4',
+    },
+    {
+      title: 'More Projects',
+      href: 'https://evanrobbymacharia.netlify.app/',
+      dates: '',
+      active: true,
+      description: 'Explore many more projects here.',
+      technologies: [],
+      links: [
+        {
+          type: 'Website',
+          href: 'https://evanrobbymacharia.netlify.app/',
+          icon: <Icons.globe className='size-3' />,
+        },
+        {
+          type: 'Source',
+          href: 'https://github.com/magicuidesign/magicui',
+          icon: <Icons.github className='size-3' />,
+        },
+      ],
+      image: '',
+      video: 'https://cdn.nyxbui.design/more-projects.mp4',
+    },
+  ],
 };
